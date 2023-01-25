cfe70c67312bed2a5a75f9d172439e60ec67b0d5
Nizar Sbai <nizar1.sbai@gmail.com>
2023-01-25 17:29:50 +0000

    Initial commit

---
+++ src/main/java/io/sbai/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package io.sbai.customerservice.repository;
+
+import io.sbai.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+
+}
---
+++ src/test/java/io/sbai/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sbai.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
